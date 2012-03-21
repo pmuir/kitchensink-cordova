5e68375ce3acaaa2df80de3b5a0e53b5a929468d
kborchers <kris.borchers@gmail.com>
2012-03-21 09:49:08 -0500

    Add iOS and Android apps

---
+++ android/bin/KitchensinkCordova-release-unsigned.apk.d
@@ -0,0 +1,5 @@
+/Users/krisborchers/git-repo/kitchensink-cordova/android/bin/KitchensinkCordova-release-unsigned.apk : \
+/Users/krisborchers/git-repo/kitchensink-cordova/android/bin/KitchensinkCordova.ap_ \
+/Users/krisborchers/git-repo/kitchensink-cordova/android/bin/classes.dex \
+/Users/krisborchers/git-repo/kitchensink-cordova/android/src/org/jboss/aerogear/kitchensink/cordova/KitchensinkCordova.java-e \
+/Users/krisborchers/git-repo/kitchensink-cordova/android/libs/cordova-1.5.0.jar \
---
+++ android/bin/classes.dex.d
@@ -0,0 +1,2 @@
+/Users/krisborchers/git-repo/kitchensink-cordova/android/bin/classes.dex : \
+/Users/krisborchers/git-repo/kitchensink-cordova/android/libs/cordova-1.5.0.jar \
