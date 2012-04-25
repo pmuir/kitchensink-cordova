60d738125fe43547c5551b5fd73b7633114779ed
kborchers <kris.borchers@gmail.com>
2012-04-25 13:28:36 -0500

    Redo Cordova 1.6.1 upgrade for Android and rebuild

--- android/gen/org/jboss/aerogear/BuildConfig.java
+++ android/gen/org/jboss/aerogear/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package org.jboss.aerogear.kitchensinkcordova;
+package org.jboss.aerogear;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
